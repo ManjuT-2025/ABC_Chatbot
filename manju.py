f35ecedd36e1d3a5766efaf74d997cfa31265839
Manju Thomas <manjuta009929@gmail.com>
2025-08-08 11:04:25 -0500

    Update manju.py

--- manju.py
+++ manju.py
@@ -54,8 +54,9 @@ if st.button("Analyze"):
     else:
       if input_text != 'stop':
         response = chat.send_message_stream(input_text)
-        response_text = "".join([part.text for part in response if hasattr(part, 'text')])
-        st.markdown(response_text, unsafe_allow_html=True)
+        response_text = response.text
+        #response_text = "".join([part.text for part in response if hasattr(part, 'text')])
+        #st.markdown(response_text, unsafe_allow_html=True)
       else:
         print('Thank you for your conversation. Have a nice day!')
 
