5919da450e3b0f5b0aa440715d0b3d0f4a28731e
Manju Thomas <manjuta009929@gmail.com>
2025-08-08 11:36:39 -0500

    Update manju.py

--- manju.py
+++ manju.py
@@ -75,7 +75,8 @@ if st.button("Analyze"):
 
   except Exception as e:
       st.write(f"Error: {e}")
-
+"""
 for message in chat.get_history():
       st.write(f'role - {message.role}',end=": ")
       st.write(message.parts[0].text)
+"""
